78212db20cf0b75ce15c892b85eee4de6bdce97f
Saurabh Patel <sp993672@gmail.com>
2024-05-25 23:30:46 +0530

    Add files via upload

---
+++ Question 6.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	printf("Saurabh patel\nRoll no.=2303510100090\n");
+	int a,b;
+	printf("Enter the first number :");
+	scanf("%d",&a);
+	printf("Enter the second number :");
+	scanf("%d",&b);
+	printf("Sum of two number :%d",a+b);
+	return 0;
+}
